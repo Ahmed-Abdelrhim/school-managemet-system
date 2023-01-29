fa4fc66a93d30de86cffe6e7eaabcbe17a0276b4
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-01-29 14:24:12 +0200

    update

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function UserView()
     {
-        // $allData = User::all();
+        return $allData = User::all();
         $data['allData'] = User::query()->where('usertype', 'Admin')->get();
         return view('backend.user.view_user', $data);
 
